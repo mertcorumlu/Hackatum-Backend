37cc69bfddf959068ca772a074a6d6744776bd26
Mert Corumlu <mert.corumlu@tum.de>
2022-11-18 23:43:02 +0100

    Initial commit

---
+++ src/main/kotlin/de/tum/hack/Bloomberg/Challenge/BloombergChallengeApplication.kt
@@ -0,0 +1,11 @@
+package de.tum.hack.Bloomberg.Challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BloombergChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BloombergChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/de/tum/hack/Bloomberg/Challenge/BloombergChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tum.hack.Bloomberg.Challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BloombergChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
