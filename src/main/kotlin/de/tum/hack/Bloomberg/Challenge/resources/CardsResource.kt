8ab4c0f31b47086679fba5bb987b0c7f418eefde
Mert Corumlu <mert.corumlu@tum.de>
2022-11-19 09:23:06 +0100

    test

--- src/main/kotlin/de/tum/hack/Bloomberg/Challenge/resources/CardsResource.kt
+++ src/main/kotlin/de/tum/hack/Bloomberg/Challenge/resources/CardsResource.kt
@@ -2,15 +2,11 @@ package de.tum.hack.Bloomberg.Challenge.resources
 
 import de.tum.hack.Bloomberg.Challenge.models.Card
 import de.tum.hack.Bloomberg.Challenge.services.CardsService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
-@RestController("/api/cards")
+@RestController
+@RequestMapping("/api/cards")
 class CardsResource(val cardsService: CardsService) {
 
     @GetMapping("")
