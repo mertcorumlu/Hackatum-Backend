6cf8e48488e0a590fefb0f732819c317073af1e7
botrufus <64660684+botrufus@users.noreply.github.com>
2022-11-19 11:55:38 +0100

    fix usercardrepo

--- src/main/kotlin/de/tum/hack/Bloomberg/Challenge/repositories/UserCardRepository.kt
+++ src/main/kotlin/de/tum/hack/Bloomberg/Challenge/repositories/UserCardRepository.kt
@@ -3,6 +3,6 @@ package de.tum.hack.Bloomberg.Challenge.repositories;
 import de.tum.hack.Bloomberg.Challenge.models.UserCard
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserCardRepository : JpaRepository<UserCard, String> {
+interface UserCardRepository : JpaRepository<UserCard, Int> {
     fun findAllCardByUserId(userId: Int): List<UserCard>
 }
