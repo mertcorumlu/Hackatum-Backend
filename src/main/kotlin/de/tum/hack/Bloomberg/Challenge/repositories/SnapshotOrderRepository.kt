c2539e579daf126a16be72add72496e3f2bec55c
Mert Corumlu <mert.corumlu@tum.de>
2022-11-20 04:53:23 +0100

    fix find buyers sellers

--- src/main/kotlin/de/tum/hack/Bloomberg/Challenge/repositories/SnapshotOrderRepository.kt
+++ src/main/kotlin/de/tum/hack/Bloomberg/Challenge/repositories/SnapshotOrderRepository.kt
@@ -10,14 +10,16 @@ interface SnapshotOrderRepository : JpaRepository<SnapshotOrder, Int> {
     @Query("""
         select s from SnapshotOrder s 
         where s.masterOrder.card.cardId = :cardId and s.masterOrder.user.id <> :user_id and
-        s.masterOrder.price >= :price order by s.masterOrder.price
+        s.masterOrder.price >= :price and s.masterOrder.side = 'BUY' 
+        order by s.masterOrder.price
         """)
     fun findBuyersForSeller(cardId: String, user_id: Int, price: Double): List<SnapshotOrder>
 
     @Query("""
         select s from SnapshotOrder s 
         where s.masterOrder.card.cardId = :cardId and s.masterOrder.user.id <> :user_id and
-        s.masterOrder.price <= :price order by s.masterOrder.price desc
+        s.masterOrder.price <= :price and s.masterOrder.side = 'SELL'  
+        order by s.masterOrder.price desc
         """)
     fun findSellersForBuyer(cardId: String, user_id: Int, price: Double): List<SnapshotOrder>
 
